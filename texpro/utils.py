e66a178b989c766bae8873dc138dcb2e39dff79a
Christoph Semken <email>
2020-06-16 19:40:31 +0200

    added update_example function

--- texpro/utils.py
+++ texpro/utils.py
@@ -1,3 +1,6 @@
+import os
+import subprocess
+from contextlib import contextmanager
 from pathlib import Path
 from warnings import warn
 
@@ -36,3 +39,73 @@ def tree(dir_path: Path, prefix: str=''):
             extension = branch if pointer == tee else space
             # i.e. space because last, └── , above so no more |
             yield from tree(path, prefix=prefix+extension)
+
+
+@contextmanager
+def cwd(path):
+    """Temporarily change the working directory"""
+    oldpwd = os.getcwd()
+    os.chdir(path)
+    try:
+        yield
+    finally:
+        os.chdir(oldpwd)
+
+
+def run_notebook(notebook: Path, kernel: str, allow_errors=False):
+    """Executes and saves a jupyter notebook in its parent folder"""
+    import nbformat
+    from nbconvert.preprocessors import ExecutePreprocessor, CellExecutionError
+
+    with cwd(notebook.parent):
+        with open(notebook.name) as f:
+            nb = nbformat.read(f, as_version=4)
+        ep = ExecutePreprocessor(kernel_name=kernel)
+        try:
+            ep.preprocess(nb)
+        except CellExecutionError:
+            if allow_errors:
+                # error is already printed
+                pass
+            else:
+                raise
+        with open(notebook.name, 'w', encoding='utf-8') as f:
+            nbformat.write(nb, f)
+
+
+def update_example(folder=Path('example'), build_folder='.tex-build', run_nb=True, kernel='pycharm-baac2d74',
+                   tex_cmd='xelatex -synctex=1 -interaction=nonstopmode -output-directory={build_folder} {file}.tex'):
+    """Updates the example. Assumes working directory is the project directory."""
+    from pdf2image import convert_from_path
+
+    assert Path('README.md').is_file(), 'not in main project directory (containing README.md)'
+    # make .tex-build folder
+    (folder / build_folder).mkdir(exist_ok=True)
+
+    # run demo notebook
+    if run_nb:
+        run_notebook(folder / 'texpro_demo.ipynb', kernel=kernel, allow_errors=True)
+
+    # build document twice
+    tex_cmd = tex_cmd.format(build_folder=build_folder, file='texpro_demo')
+    run_args = dict(stdout=subprocess.PIPE, stderr=subprocess.STDOUT, shell=True, text=True)
+    with cwd(folder):
+        for i in range(2):
+            result = subprocess.run(tex_cmd, **run_args)
+            if result.returncode != 0:
+                raise Exception('Error compiling document.\n'
+                                f'Command: {result.args}\n{result.stdout}')
+
+    # move pdf, delete build folder
+    pdf = folder / 'texpro_demo.pdf'
+    (folder / build_folder / 'texpro_demo.pdf').replace(pdf)
+    # (folder / build_folder).rmdir()
+
+    # convert to image
+    convert_from_path(pdf)[0].save(str(folder / 'texpro_demo.png'))
+
+    # re-run demo notebook and convert to markdown
+    if run_nb:
+        run_notebook(folder / 'texpro_demo.ipynb', kernel=kernel)
+
+    # TODO paste markdown into readme
