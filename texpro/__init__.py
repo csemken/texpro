53a97a71b3e3ab8d8528fbffac4676fa23cee014
csemken <csemken@users.noreply.github.com>
2022-08-02 12:28:55 +0200

    🚀 RELEASE: v0.9.2

--- texpro/__init__.py
+++ texpro/__init__.py
@@ -1,6 +1,6 @@
 """Create, view and export LaTeX assets (equations, figures, tables, etc.)"""
 
-__version__ = '0.9.1'
+__version__ = '0.9.2'
 
 from .settings import config
 from .texassets import *
